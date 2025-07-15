e9cd5fe7c0e7820df184a133cc242c2379617e60
Pawel S <PAWSI@sokolow.pl>
2025-07-15 14:08:48 +0200

    Book.py added

---
+++ book.py
@@ -0,0 +1,6 @@
+class Book():
+    def __init__(self,title,author,year,availability):
+        self.title=title
+        self.author=author
+        self.year=year
+        self.availability=availability

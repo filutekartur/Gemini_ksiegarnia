25b50c9ccbba26e756242b89d498c043174bcd74
Pawel S <PAWSI@sokolow.pl>
2025-07-15 14:33:17 +0200

    main.py updated with library class

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from book import Book
-
+from library import Library
+lib = Library()
 a = Book("Mine","He",2000,True)
-
-a.desc()
+b= Book("Yours","Me",1999,False)
+lib.set_book(a)
+lib.set_book(b)
+lib.get_books()

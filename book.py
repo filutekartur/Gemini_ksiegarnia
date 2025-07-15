dcd24faee907ef36c406c5e7d01cd95c6f75003b
Pawel S <PAWSI@sokolow.pl>
2025-07-15 14:21:44 +0200

    desc function added to book.py

--- book.py
+++ book.py
@@ -4,3 +4,6 @@ class Book():
         self.author=author
         self.year=year
         self.availability=availability
+
+    def desc(self):
+        print(f"The  {self.title}  book, written by  {self.author}  in  {self.year}  is {"available" if self.availability else "unavailbable"}")

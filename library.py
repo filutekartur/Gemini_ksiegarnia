602f39aa4099e1a02ebc75990ef5c6c07f657bf8
Pawel S <PAWSI@sokolow.pl>
2025-07-22 14:29:37 +0200

    new_book() function
    Added function for inputing new books to library

--- library.py
+++ library.py
@@ -1,8 +1,18 @@
+from book import Book
 class Library():
-    def __init__(self):
+    def __init__(self,name):
+        self.name = name
         self.books = []
     def get_books(self):
+        print(f"\nBooks of {self.name}:\n")
         for book in self.books:
             book.desc()
     def set_book(self,book):
         self.books.append(book)
+    def new_book(self):
+        title = input("Podaj tytuł: ")
+        author = input("Podaj autora: ")
+        year = input("Podaj rok wydania książki: ")
+        book = Book(title,author,year)
+        self.set_book(book)
+        print(f"Book {title} added to {self.name}")
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from book import Book
 from library import Library
-lib = Library()
+lib = Library("Biblioteka")
 a = Book("Mine","He",2000,True)
 b= Book("Yours","Me",1999,False)
 lib.set_book(a)
 lib.set_book(b)
+lib.new_book()
 lib.get_books()

19a40cb4cb23d6a5b9e5da32988fcdce7ed08093
Pawel S <PAWSI@sokolow.pl>
2025-08-26 19:11:52 +0200

    begining of file

---
+++ menu.py
@@ -0,0 +1,9 @@
+import os
+def menu():
+    print("Chose option: ")
+    print("1. Show books: ")
+    print("2. Add new book: ")
+    print("3. Search book: ")
+    print("4. Borrow/Return book: ")
+    print("5. Exit: ")
+    choice = int(input(""))
